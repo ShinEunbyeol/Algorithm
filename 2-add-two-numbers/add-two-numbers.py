e7c01a8feaeb9fc59c6b7b0ded52ea94e3808659
SilverStar <80660413+ShinEunbyeol@users.noreply.github.com>
2023-12-14 00:49:02 +0900

    Time: 69 ms (34.40%) | Memory: 16.4 MB (47.93%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -7,37 +7,20 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         result = ListNode()
-        result.head = None
+        curr = result
         carry = 0
-        while l1 and l2:
-            sum = l1.val + l2.val + carry
-            self.appendNode(result, sum % 10)
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            sum = l1Val + l2Val + carry
 
-            carry = 0
-            if sum >= 10:
-                carry = 1
+            newNode = ListNode(sum % 10)
+            curr.next = newNode
+            curr = newNode
 
-            l1 = l1.next
-            l2 = l2.next
+            carry = sum // 10
 
-            if l1 == None and l2 != None:
-                l1 = ListNode(0)
-            if l2 == None and l1 != None:
-                l2 = ListNode(0)
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
         
-        if carry != 0:
-            self.appendNode(result, carry)
-        
-        return result
-
-    def appendNode(self, list, val):
-        newNode = ListNode(val)
-        if list.head is None:
-            list.val = val
-            list.next = None
-            list.head = newNode
-            return
-
-        while list.next:
-            list = list.next
-        list.next = newNode
+        return result.next
